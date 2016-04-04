e1ae7ab29d720eff742135e70cf368bc8d2514ff
Brian Voong <bhlvoong@gmail.com>
2016-04-04 12:14:09 -0700

    Part 1, Auto Layout in code or programmatically

---
+++ fbMessenger/ViewController.swift
@@ -0,0 +1,161 @@
+//
+//  ViewController.swift
+//  fbMessenger
+//
+//  Created by Brian Voong on 4/1/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Recent"
+        collectionView?.backgroundColor = UIColor.redColor()
+
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.alwaysBounceVertical = true
+        
+        collectionView?.registerClass(FriendCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, 100)
+    }
+}
+
+class FriendCell: BaseCell {
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .ScaleAspectFill
+        imageView.layer.cornerRadius = 34
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mark Zuckerberg"
+        label.font = UIFont.systemFontOfSize(18)
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friend's message and something else..."
+        label.textColor = UIColor.darkGrayColor()
+        label.font = UIFont.systemFontOfSize(14)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.font = UIFont.systemFontOfSize(16)
+        label.textAlignment = .Right
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .ScaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    override func setupViews() {
+        
+        addSubview(profileImageView)
+        addSubview(dividerLineView)
+        
+        setupContainerView()
+        
+        profileImageView.image = UIImage(named: "zuckprofile")
+        hasReadImageView.image = UIImage(named: "zuckprofile")
+        
+        addConstraintsWithFormat("H:|-12-[v0(68)]", views: profileImageView)
+        addConstraintsWithFormat("V:[v0(68)]", views: profileImageView)
+        
+        addConstraint(NSLayoutConstraint(item: profileImageView, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
+        
+        addConstraintsWithFormat("H:|-82-[v0]|", views: dividerLineView)
+        addConstraintsWithFormat("V:[v0(1)]|", views: dividerLineView)
+    }
+    
+    private func setupContainerView() {
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        addConstraintsWithFormat("H:|-90-[v0]|", views: containerView)
+        addConstraintsWithFormat("V:[v0(50)]", views: containerView)
+        addConstraint(NSLayoutConstraint(item: containerView, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0))
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        containerView.addConstraintsWithFormat("H:|[v0][v1(80)]-12-|", views: nameLabel, timeLabel)
+        
+        containerView.addConstraintsWithFormat("V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+        
+        containerView.addConstraintsWithFormat("H:|[v0]-8-[v1(20)]-12-|", views: messageLabel, hasReadImageView)
+        
+        containerView.addConstraintsWithFormat("V:|[v0(24)]", views: timeLabel)
+        
+        containerView.addConstraintsWithFormat("V:[v0(20)]|", views: hasReadImageView)
+    }
+    
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerate() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.blueColor()
+    }
+}
+
